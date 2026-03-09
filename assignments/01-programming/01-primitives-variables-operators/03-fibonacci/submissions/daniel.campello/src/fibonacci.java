1b9a04ae8db7c7aee688dac0bcff6e82b10e8440
DanielCMBC <143235356+DanielCMBC@users.noreply.github.com>
2026-03-09 18:22:48 +0000

    fibonacci

---
+++ assignments/01-programming/01-primitives-variables-operators/03-fibonacci/submissions/daniel.campello/src/fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro não negativo: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1, c;
+        System.out.print("O " + n + "º número da sequência de Fibonacci é: ");
+        if (n == 0) {
+            System.out.println(a);
+        } else if (n == 1) {
+            System.out.println(b);
+        } else {
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println(b);
+        };
+    }
+}
