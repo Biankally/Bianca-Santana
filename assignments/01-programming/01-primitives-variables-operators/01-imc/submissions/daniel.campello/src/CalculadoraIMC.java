2663c95bb62da6b016672ef6993ec3520d8afe4f
DanielCMBC <143235356+DanielCMBC@users.noreply.github.com>
2026-03-10 17:13:40 +0000

    DOR E SOFRIMENTO

--- assignments/01-programming/01-primitives-variables-operators/01-imc/submissions/daniel.campello/src/CalculadoraIMC.java
+++ assignments/01-programming/01-primitives-variables-operators/01-imc/submissions/daniel.campello/src/CalculadoraIMC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class imc {
+public class CalculadoraIMC {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- assignments/01-programming/01-primitives-variables-operators/02-area/submissions/daniel.campello/src/CalcularPoligono.java
+++ assignments/01-programming/01-primitives-variables-operators/02-area/submissions/daniel.campello/src/CalcularPoligono.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class area {
+public class CalcularPoligono {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
