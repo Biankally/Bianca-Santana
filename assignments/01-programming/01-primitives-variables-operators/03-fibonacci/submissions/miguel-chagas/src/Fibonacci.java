d1d1075ea69042017d5f2cd0c8d78c77120f4684
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-03-11 08:26:57 -0300

    feat: novas tarefas (1 a 4)

---
+++ assignments/01-programming/01-primitives-variables-operators/03-fibonacci/submissions/miguel-chagas/src/Fibonacci.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+
+    static long atual = 1;
+    static long n1 = 1;
+    static long n2 = 0;
+    static long aux = 0;
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro não negativo: ");
+        int n = scan.nextInt();
+
+        System.out.printf("\n%s",formatarSaida(calcularFibonacci(n),n));
+
+        scan.close();
+
+    }
+
+    public static long calcularFibonacci(long n){
+
+        if(n == 1 || n == 2) return 1;
+
+        return calcularFibonacci(n - 2) + calcularFibonacci(n - 1);
+    }
+
+    public static String formatarSaida(long fibonacci, int n){
+        return String.format("O %dº número da sequência de Fibonacci é: %d\n", n, fibonacci);
+    }
+}
