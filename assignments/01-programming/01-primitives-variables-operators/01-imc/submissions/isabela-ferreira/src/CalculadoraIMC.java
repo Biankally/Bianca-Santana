85043cc45e43f9c3baab174a42ff754e0fc7a713
Arkis <isabelaarkis@gmail.com>
2026-03-12 20:06:20 -0300

    .

--- assignments/01-programming/01-primitives-variables-operators/01-imc/submissions/isabela-ferreira/src/CalculadoraIMC.java
+++ assignments/01-programming/01-primitives-variables-operators/01-imc/submissions/isabela-ferreira/src/CalculadoraIMC.java
@@ -5,10 +5,10 @@ public class CalculadoraIMC {
     public static void main(String[] args){
         Scanner scanner = new Scanner (System.in);
 
-        System.out.printf("Digite sua altura(m): ");
-        double altura = scanner.nextDouble();
         System.out.printf("Digite seu peso(kg): ");
         double peso = scanner.nextDouble();
+        System.out.printf("Digite sua altura(m): ");
+        double altura = scanner.nextDouble();
 
         scanner.close();
 
