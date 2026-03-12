da0abed4d9695b124b20aebe27f635034c4c2d45
Pedro Lucas <Pedro Lucas pedrolucaslopesmonteiro43@gmail.com>
2026-03-11 21:35:11 -0300

    Adcionando comentários nos códdigos

--- examples/02c-input-output/src/ScannerSample.java
+++ examples/02c-input-output/src/ScannerSample.java
@@ -1,14 +1,14 @@
 
-import java.util.Scanner;
+import java.util.Scanner; // importa a classe scanenr, disponivel no pacote java.util
 
-public class ScannerSample {
+public class ScannerSample { 
 
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Scanner é um tipo do java(tipo complexo) --> construiu um novo tipo de Scanner com parametro de um tipo de entrada
 
         System.out.print("Informe um inteiro: ");
-        int i = input.nextInt();
+        int i = input.nextInt(); // le o proximo valor como exemplo
         System.out.printf("Valor informado: %d%n", i); // %d inteiros
 
         System.out.print("Informe um double: ");
@@ -24,7 +24,7 @@ public class ScannerSample {
         System.out.printf("Valor informado: %b%n", b); // %b boleanos
 
         System.out.print("Informe um texto: ");
-        String s = input.next();
+        String s = input.next(); // next lê o proximo valor como string
         System.out.printf("Valor informado: %s%n", s); // %s String
 
         input.nextLine();
@@ -33,7 +33,7 @@ public class ScannerSample {
         s = input.nextLine();
         System.out.printf("Valor informado: %s%n", s); // %s String
 
-        input.close();
+        input.close(); // input close fecha o Scanner e a conexão com Sytem.in
         System.out.print("Fim");
     }
     
