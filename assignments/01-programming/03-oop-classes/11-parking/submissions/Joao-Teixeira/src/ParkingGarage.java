8791b2cedd6fc1d7f7c878ed24cba453a15d9d74
JV <joao.victormt1210@gmail.com>
2026-04-09 22:04:20 -0300

    .

--- assignments/01-programming/03-oop-classes/11-parking/submissions/Joao-Teixeira/src/ParkingGarage.java
+++ assignments/01-programming/03-oop-classes/11-parking/submissions/Joao-Teixeira/src/ParkingGarage.java
@@ -4,22 +4,21 @@ public class ParkingGarage {
 
 
     public static double calculateCharges(double hours){
-        double charge = 0;
-        if(hours <= 3) charge = 2; 
-        else if(hours > 3){
-            charge = Math.ceil(hours - 3) * 0.50;
-        }
+        double preco = 0;
+        if(hours <= 3) preco = 2; 
+    
+        
+        preco = Math.ceil(hours - 3) * 0.50;
+        
+        preco = 2.00 + Math.ceil(hours - 3) * 0.50;
 
-        if(charge > 10) charge = 10;
-
-
-        return charge;
+        return Math.min(preco, 10.00);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double total = 0.0;
-        int num_cliente = 1;
+        int num_clientes = 1;
 
         System.out.println("=== Sistema de Cobrança - Garagem de Estacionamento ===\n");
 
@@ -31,14 +30,12 @@ public class ParkingGarage {
             
             double preco = calculateCharges(hours);
 
-            System.out.printf("Cliente %d: Taxa de estacionamento: $%.2f%n", num_cliente, preco);
+            System.out.printf("Cliente %d: Taxa de estacionamento: $%.2f%n", num_clientes, preco);
             total += preco;
-            num_cliente++;
+            num_clientes++;
         }
 
         System.out.printf("%nTotal arrecadado ontem: $%.2f%n", total);
         scanner.close();
     }
 }
-    
-    
