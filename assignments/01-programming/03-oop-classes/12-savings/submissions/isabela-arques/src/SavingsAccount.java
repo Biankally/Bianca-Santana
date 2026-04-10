f5e723d6fed645b72c307525d3976aaf61876060
Arkis <isabelaarkis@gmail.com>
2026-04-09 22:16:43 -0300

    .

--- assignments/01-programming/03-oop-classes/12-savings/submissions/isabela-arques/src/SavingsAccount.java
+++ assignments/01-programming/03-oop-classes/12-savings/submissions/isabela-arques/src/SavingsAccount.java
@@ -1,34 +1,38 @@
-import java.util.Scanner; 
-
 public class SavingsAccount {
-
     private double savingsBalance;
     private static double annualInterestRate;
 
+   
     public SavingsAccount(double savingsBalance) {
-        if (savingsBalance < 0.0) {
+        if (savingsBalance < 0) {
             throw new IllegalArgumentException("O saldo inicial não pode ser negativo.");
         }
         this.savingsBalance = savingsBalance;
     }
 
-    public void calculateMonthlyInterest() {
-        double monthlyInterestRate = annualInterestRate / 12.0;
-        double interest = savingsBalance * monthlyInterestRate;
-        savingsBalance += interest;
-    }
-
-    public static void setAnnualInterestRate(double rate) {
-        if (rate < 0.0) {
+    
+    public static void setAnnualInterestRate(double annualInterestRate) {
+        if (annualInterestRate < 0) {
             throw new IllegalArgumentException("A taxa de juros não pode ser negativa.");
         }
-        annualInterestRate = rate;
+        SavingsAccount.annualInterestRate = annualInterestRate;
     }
 
+    
     public static double getAnnualInterestRate() {
         return annualInterestRate;
     }
 
+
+    public void calculateMonthlyInterest() {
+        if (annualInterestRate == 0) {
+            return;
+        }
+        double multiplier = Math.pow(1.0 + annualInterestRate / 100.0, 1.0 / 12.0);
+        savingsBalance *= multiplier;
+    }
+
+   
     public double getSavingsBalance() {
         return savingsBalance;
     }
