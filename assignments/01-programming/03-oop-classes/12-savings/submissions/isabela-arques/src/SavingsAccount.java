e6410c7b60a0e7d12952a7f7f220a9872d4a6123
Arkis <isabelaarkis@gmail.com>
2026-04-09 22:02:07 -0300

    .

---
+++ assignments/01-programming/03-oop-classes/12-savings/submissions/isabela-arques/src/SavingsAccount.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SavingsAccount {
+
+    private double savingsBalance;
+    private static double annualInterestRate;
+
+    public SavingsAccount(double savingsBalance) {
+        if (savingsBalance < 0.0) {
+            throw new IllegalArgumentException("O saldo inicial não pode ser negativo.");
+        }
+        this.savingsBalance = savingsBalance;
+    }
+
+    
+    public void calculateMonthlyInterest() {
+        if (annualInterestRate < 0.0) {
+           
+            throw new IllegalArgumentException("Taxa de juros inválida.");
+        }
+        double monthlyInterestRate = annualInterestRate / 12.0;
+        double interest = savingsBalance * monthlyInterestRate;
+        savingsBalance += interest;
+    }
+
+  
+    public static void setAnnualInterestRate(double rate) {
+        if (rate < 0.0) {
+            throw new IllegalArgumentException("A taxa de juros não pode ser negativa.");
+        }
+        annualInterestRate = rate;
+    }
+
+   
+    public static double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+}
