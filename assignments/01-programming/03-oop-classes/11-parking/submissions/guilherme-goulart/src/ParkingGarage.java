2b6e5c08ec3d8f5021e869336c74c45954462bd9
Guilherme0421 <guigao.goulart@gmail.com>
2026-04-15 11:49:15 -0300

    Parking

---
+++ assignments/01-programming/03-oop-classes/11-parking/submissions/guilherme-goulart/src/ParkingGarage.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ParkingGarage {
+    public static double calculateCharges(double hours) {
+        double charge = 0.0;
+        if (hours <= 3) {
+            charge = 2.0;
+        } else if (hours > 3 && hours <= 24) {
+            charge = 2.0 + (hours - 3) * 0.5;
+        } else {
+            charge = 10.0;
+        }
+        return charge;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double totalArrecadado = 0.0;
+        int clienteCount = 0;
+
+        while (true) {
+            System.out.print("Digite o número de horas estacionadas para o cliente (ou -1 para sair): ");
+            double hoursParked = scanner.nextDouble();
+
+            if (hoursParked == -1) {
+                break;
+            }
+
+            double currentCharge = calculateCharges(hoursParked);
+            System.out.println("Cliente " + (++clienteCount) + ": Taxa de estacionamento: $" + currentCharge);
+            totalArrecadado += currentCharge;
+        }
+        System.out.println("Total arrecadado ontem: $" + totalArrecadado);
+    }
+}
