dab69b7efde6f55bbb5a0670932a29363db62de8
Guilherme0421 <guigao.goulart@gmail.com>
2026-04-15 14:41:49 -0300

    Savings

---
+++ assignments/01-programming/03-oop-classes/12-savings/submissions/guilherme-goulart/src/SavingsAccount.java
@@ -0,0 +1,33 @@
+public class SavingsAccount {
+    private double savingsBalance;
+    private static double annualInterestRate;
+
+    // Método construtor
+    public SavingsAccount(double savingsBalance){
+        if(savingsBalance < 0){
+            throw new IllegalArgumentException("O saldo inicial não pode ser negativo.");
+        }
+        this.savingsBalance = savingsBalance;
+    }
+
+    public void calculateMonthlyInterest(){
+        double monthlyRate = (savingsBalance *(annualInterestRate / 100)) / 12;
+        this.savingsBalance += monthlyRate;
+    }
+
+    public static void setAnnualInterestRate(double rate) {
+        if(rate < 0) {
+            throw new IllegalArgumentException("A taxa de juros não pode ser negativa.");
+        }
+        annualInterestRate = rate;
+    }
+
+    public static double getAnnualInterestRate(){
+        return annualInterestRate;
+    }
+
+    public double getSavingsBalance(){
+        return savingsBalance;
+    }
+    
+}
